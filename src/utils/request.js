7ea162fb5b6adbc4538836d806365f5f4a3543a1
supertinypeaunt <1176032017@qq.com>
2019-11-30 22:44:33 +0800

    请求默认配置完成

---
+++ src/utils/request.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import JSONBig from 'json-bigint'
+
+// 设置请求头
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+// 响应拦截器
+request.interceptors.response.use((response) => {
+  // 处理响应数据id过大，使用json-bigint处理
+  try {
+    return JSONBig.parse(response)
+  } catch (err) {
+    // 捕获JSONBig.parse无法解析非对象数据，而报错
+    return {}
+  }
+})
