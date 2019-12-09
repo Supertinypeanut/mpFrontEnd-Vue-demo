6b07e752e3a9ba43475fb94aaf314eb695389ce7
supertinypeaunt <1176032017@qq.com>
2019-12-09 23:11:00 +0800

    开发混合应用需要配置vue.comfig.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /**
+    * / 用于部署在 HTTP 服务中
+    * 如果是混合应用，则将其设置为相对路径 ./
+    * 参考文档：https://cli.vuejs.org/zh/config/#publicpath
+    */
+  publicPath: './',
+
+  devServer: {
+    // host: '和手机在一个网段的网卡地址'
+  }
+}
